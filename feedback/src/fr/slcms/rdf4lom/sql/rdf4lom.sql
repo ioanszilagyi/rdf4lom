fc07403b1f086ceb8b9773b61082548630f2ce59
itfradu <itfradu@ed9af680-adcd-11dd-82eb-ff4e721a02f4>
2009-02-18 16:32:53 +0000

    Baza de date pe care o foloseste aceasta veriune se afla in: rdf4lom/src/fr/slcms/rdf4lom/sql/rdf4lom.sql

---
+++ feedback/src/fr/slcms/rdf4lom/sql/rdf4lom.sql
@@ -0,0 +1,49 @@
+# SQL Manager 2007 Lite for MySQL 4.4.2.1
+# ---------------------------------------
+# Host     : localhost
+# Port     : 3306
+# Database : rdf4lom
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES latin1 */;
+
+SET FOREIGN_KEY_CHECKS=0;
+
+CREATE DATABASE `rdf4lom`
+    CHARACTER SET 'latin1'
+    COLLATE 'latin1_swedish_ci';
+
+USE `rdf4lom`;
+
+#
+# Structure for the `comments` table : 
+#
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comment` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for the `visits` table : 
+#
+
+CREATE TABLE `visits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `organisation` varchar(150) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
